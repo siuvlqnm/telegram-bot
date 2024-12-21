524499a36ac231fbb5aa594982897140cd04b490
siuvlqnm <siuvlqnm@users.noreply.github.com>
2024-12-21 15:39:26 +0800

    增加OpenRouter接口

--- src/services/openrouter.ts
+++ src/services/openrouter.ts
@@ -1,16 +1,15 @@
 import { OpenAI } from 'openai';
 import { OPENROUTER_API_KEY } from '@/config';
 
-const openai = new OpenAI({
-  baseURL: "https://openrouter.ai/api/v1",
-  apiKey: OPENROUTER_API_KEY(),
-  defaultHeaders: {
-    "X-Title": "Telegram Bot", // Optional. Shows in rankings on openrouter.ai.
-  }
-});
-
 export async function getAiResponse(messages: any[]) {
   try {
+    const openai = new OpenAI({
+      baseURL: "https://openrouter.ai/api/v1",
+      apiKey: OPENROUTER_API_KEY(),
+      defaultHeaders: {
+        "X-Title": "Telegram Bot", // Optional. Shows in rankings on openrouter.ai.
+      }
+    });
     const completion = await openai.chat.completions.create({
        model: 'google/gemini-2.0-flash-thinking-exp:free',
        messages: messages,
